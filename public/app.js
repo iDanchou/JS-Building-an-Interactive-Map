bd56931ae15f45114d8e1ef09995a2bf678f85f2
Rasheen Kirkland <kirkland.rasheen@gmail.com>
2024-02-11 11:25:37 -0800

    assignment

--- public/app.js
+++ public/app.js
@@ -2,25 +2,35 @@
 
 
 
-// Create map
+// Create map:                                                       
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
 
+// Add OpenStreetMap tiles:
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+}).addTo(myMap)
 
-
-
-// Add openstreetmap tiles
-
-
+// Create and add a geolocation marker:
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 //Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
 
+// Draw the 2nd Arrondissement                                          
+var latlngs = [[48.863368120198004, 2.3509079846928516],
+[48.86933262048345, 2.3542531602919805],
+[48.87199261164275, 2.3400569901592183],
+[48.86993336274516, 2.3280142476578813], 
+[48.86834104280146, 2.330308418109664]]
 
-// Create and add a geolocation marker
-
-
-
-
-// Draw the 2nd arrondissement
-
+var polygon = L.polygon(latlngs, {
+    color: 'blue',
+    fillOpacity: 0.2
+}).addTo(myMap);
 
 
 
